7c99b9af49bb7a51ad8775ddf47e5cab10cf9389
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-11 00:13:27 +0530

    Add project files.

---
+++ CabInvoiceGeneratorDemo/RideType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGeneratorDemo
+{
+    public enum RideType
+    {
+        NORMAL,
+        PREMIUM
+    }
+}
