f7c55f49c4828eec9faa714c53166468a6c877c0
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-11 20:54:10 +0530

    [Shantanu] Added : Main method and Printing Welcome message on Console Window.

--- CabInvoiceGeneratorDemo/Program.cs
+++ CabInvoiceGeneratorDemo/Program.cs
@@ -9,15 +9,8 @@ namespace CabInvoiceGeneratorDemo
             Console.WriteLine("Welcome to Cab Invoice Generator");
 
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
-            RideRepository rideRepository = new RideRepository();
-            InvoiceSummary invoiceSummary;
-
             double singleFare = invoiceGenerator.CalculateFare(2.0, 5);
             Console.WriteLine($"Single Ride Fare : {singleFare}");
-
-            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 5) };
-            invoiceGenerator.CalculateFare(rides);
-            invoiceSummary.averageFare;
         }
     }
 }
